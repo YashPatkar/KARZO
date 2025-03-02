21fbc4188c8ca1780cc919be0c148d4d126910c2
Yash Mahesh Patkar <yash.patkar2004@gmail.com>
2025-03-02 14:07:02 +0530

    Remove deprecated driver registration and OTP verification functions; streamline email handling process

--- backend/driver/views.py
+++ backend/driver/views.py
@@ -8,53 +8,7 @@ from .utils import generate_otp, send_email_otp, verify_email
 from django.core.cache import cache
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-@api_view(['POST'])
-def DriverRegister(request):
-    data = request.data
-    email = data.get('email')
 
-    if not email:
-        return Response({"error": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
-
-    if not verify_email(email):
-        return Response({"error": "Invalid or undeliverable email address"}, status=status.HTTP_400_BAD_REQUEST)
-
-    email_otp = generate_otp()
-    print("Generated OTP:", email_otp)  # Debugging purpose
-
-    # Store OTP in the session
-    request.session['email'] = email
-    request.session['email_otp'] = email_otp
-    request.session.set_expiry(300)  # Session expires after 5 minutes (300 seconds)
-    request.session.modified = True  # Force session to save
-    request.session.save()
-
-    # Retrieve OTP from the session
-    session_email = request.session.get('email')
-    session_otp = request.session.get('email_otp')
-
-    print("Session Email:", session_email)  # Debugging purpose
-    print("Session OTP:", session_otp)  # Debugging purpose
-    print('---------------------------')
-
-    send_email_otp(email, email_otp)
-
-    return Response({"message": "OTP sent successfully"}, status=status.HTTP_200_OK)
-
-@csrf_exempt
-@api_view(['POST'])
-def DriverRegisterVerifyOTP(request):
-    data = request.data
-    email = data.get('email')
-    email_otp = data.get('email_otp')
-    # Retrieve OTP from the session
-    if email_otp and email:
-        request.session.pop('email', None)
-        request.session.pop('email_otp', None)
-        # Skiping the OTP verification for now 
-        return Response({"message": "OTP verified successfully"}, status=status.HTTP_200_OK)
-    return Response({"error": "Invalid OTP"}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def AssignDriver(self, request, event_id, driver_id):
